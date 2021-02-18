9d0ebe3a0d9e822aee159e3e688ef715f3850ad9
Nikolay Tyurin <kolya_tyurin@mail.ru>
2021-02-19 01:14:49 +0300

    new version strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e410ffc5a538ae3e9796ad616140ae73'),
+    },
+  },
+});
